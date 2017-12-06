c248a3f8b87bdf26ae41589f77f1b9db93b683a2
Mollie Maarten <maarten.mollie@student.howest.be>
2017-12-06 11:32:35 +0100

    Centered

--- create3/assets/scripts/script.js
+++ create3/assets/scripts/script.js
@@ -1,6 +1,6 @@
 var appid = "0631934e";
 var appkey = "bb134db29440aa5047d2e10211941e4e";
-var zoekDitRecept = '';
+var zoekDitRecept ='';
 var veggie =  "";
 var pesc = "";
 var randomrecipe;
@@ -20,19 +20,19 @@ function doCall(){
                url: "https://api.yummly.com/v1/api/recipe/"+res.matches[randomrecipe].id+"?_app_id="+appid+"&_app_key="+appkey,
                success: function (response) {
                     $('.jumbotronText h1').html(response.name);
-                    $('.jumbotronText .divspan figure:nth-child(1) span').html("COOK: " + response.totalTimeInSeconds/60 + " mins");
+                    $(".divspan figure #cook>span" ).html(response.totalTimeInSeconds/60 + " Minutes");
                     $('.jumbotron>figure>img').attr("src",response.images[0].hostedLargeUrl);
                     $('.ingredient-list').empty();
                     for(var i in response.ingredientLines){
                         $('.ingredient-list').append("<li>" + response.ingredientLines[i] + "</li>");
                     }
                     if(response.nutritionEstimates.length == 0){
-                        $(".divspan #cal").hide();
+                        $(".divspan #hidecal").hide();
                     } else{
-                        $(".divspan #cal>span").html(response.nutritionEstimates[randomrecipe].value + " CALORIES");
+                        $(".divspan figure #cal>span").html(response.nutritionEstimates[randomrecipe].value + " CALORIES");
                     }
-                    $(".divspan #serves>span").html("SERVINGS " + response.numberOfServings);
-                    $("#recipe").html("<a target='_blank' href='"+response.source.sourceRecipeUrl+"'>Bekijk origineel recept</a>");
+                    $(".divspan #serves>span").html(response.numberOfServings);
+                    $("#recipelink").html("<a target='_blank' href='"+response.source.sourceRecipeUrl+"'>Bekijk origineel recept</a>");
                }
            });
 
